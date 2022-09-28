4222b861fa8c63e5915fc4c0bcf7f7e602e6af34
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-28 18:14:51 -0300

    Removido metodo de sprint desse arquivo e passado para o main

--- main/sensorReader.ino
+++ main/sensorReader.ino
@@ -1,6 +1,6 @@
 void readS()
 {
-  int f = 120;
+  int f = 150;
   s1 = analogRead(A0) < f;
   s2 = analogRead(A1) < f;
   s3 = analogRead(A2) < f;
@@ -10,9 +10,9 @@ void readS()
   s7 = analogRead(A6) < f;
   s8 = analogRead(A7) < f;
   s9 = analogRead(A8) < f;
-  Serial.print(analogRead(A0));Serial.print(" ");Serial.print(analogRead(A1));Serial.print(" ");Serial.print(analogRead(A2));Serial.print(" ");
-  Serial.print(analogRead(A3));Serial.print(" ");Serial.print(analogRead(A4));Serial.print(" ");Serial.print(analogRead(A5));Serial.print(" ");
-  Serial.print(analogRead(A6));Serial.print(" ");Serial.print(analogRead(A7));Serial.print(" ");Serial.print(analogRead(A8));Serial.print("\n");
+  // Serial.print(analogRead(A0));Serial.print(" ");Serial.print(analogRead(A1));Serial.print(" ");Serial.print(analogRead(A2));Serial.print(" ");
+  // Serial.print(analogRead(A3));Serial.print(" ");Serial.print(analogRead(A4));Serial.print(" ");Serial.print(analogRead(A5));Serial.print(" ");
+  // Serial.print(analogRead(A6));Serial.print(" ");Serial.print(analogRead(A7));Serial.print(" ");Serial.print(analogRead(A8));Serial.print("\n");
 }
 
 void error()
@@ -55,36 +55,4 @@ void errorI()
   else if ( s1 &&  s2 &&  s3 &&  s4 &&  s5 &&  s6 &&  s7 && !s8 &&  s9) {erro =  6;}  // Sensor 8     na linha branca
   else if ( s1 &&  s2 &&  s3 &&  s4 &&  s5 &&  s6 &&  s7 && !s8 && !s9) {erro =  7;}  // Sensor 8 e 9 na linha branca
   else if ( s1 &&  s2 &&  s3 &&  s4 &&  s5 &&  s6 &&  s7 &&  s8 && !s9) {erro =  8;}  // Sensor 9     na linha branca
-}
-
-
-void sprint ()
-{
-  P = erro;
-  I = I + erro;
-  D = erro - erro_antes;
-  PID = (Kp * P) + (Kd * D) + (Ki * I);
-  
-  speedL = 247 + PID;
-  speedR = 247 - PID;
-
-  if(speedR < 0)
-    speedR = 0;  
-  else if(speedR > 255)
-    speedR = 255;
-  if(speedL < 0)
-    speedL = 0;
-  else if(speedL > 255)
-    speedL = 255;
-    
-  analogWrite(motorR, speedR);
-  analogWrite(motorL, speedL);
-}
-
-void desafios(){
-  if((!s5 || !s4|| !s6) && s9 && s1 && s8 && s7 && (faixa == false)){
-    faixa = true;
-    while(faixa == true)    
-      faixaPedestre();
-  }
 }
