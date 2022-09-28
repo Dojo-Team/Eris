4443b827deff89984f495f258ae3b49291cb7088
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-28 18:13:31 -0300

    Codigo da rotatoria

--- main/roundabout.ino
+++ main/roundabout.ino
@@ -1,4 +1,18 @@
-void roundbout()
+void roundabout(int marcacoes)
 {
-	
+  int contador = 1;
+  bool lastWhite = false;
+  while(contador != marcacoes){
+    readAndSprint();
+    if(!s9 && lastWhite)
+      lastWhite = false;
+    else if(s9 && !lastWhite)
+      contador++;
+  }
+  readAndSprint();
+  delay(100);
+  analogWrite(motorR, 20);
+  analogWrite(motorL, 150);
+  delay(100);
+  readAndSprint();
 }
