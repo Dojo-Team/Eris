f9c107430421b3c7748bdb1d5fa7a65ca0061f7c
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-28 09:14:35 -0300

    Constantes

--- main/main.ino
+++ main/main.ino
@@ -8,7 +8,7 @@ boolean s1, s2, s3, s4, s5, s6, s7, s8, s9;
 int motorR = 2, motorL = 7;
 
 //Constantes PID
-float Kp = 20, Ki = 0.3, Kd = 10;
+float Kp = 30, Ki = 0.3, Kd = 20;
 
 //Variaveis para calcular o PID
 float P = 0, I = 0, D = 0, PID = 0;
@@ -18,7 +18,7 @@ int erro = 0, erro_antes = 0;
 boolean faixa = false;
 
 //Velocidades iniciais dos motores
-int speedR = 100, speedL = 100;
+int speedR = 255, speedL = 255;
 
 void setup() {
     //MOTORES
@@ -30,18 +30,22 @@ void setup() {
     Serial.begin(9600);
 
     //MOTORES SEMPRE ANDAM PARA FRENTE
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(motorR, speedR);
+    analogWrite(motorL, speedL);   
+    delay(1000); 
+    speedR = 220;
+    speedL = 220;
 }
 
 void loop() {
     readS();
-    Serial.print(s1);Serial.print(" ");Serial.print(s2);Serial.print(" ");Serial.print(s3);Serial.print(" ");
-    Serial.print(s4);Serial.print(" ");Serial.print(s5);Serial.print(" ");Serial.print(s6);Serial.print(" ");
-    Serial.print(s7);Serial.print(" ");Serial.print(s8);Serial.print(" ");Serial.print(s9);Serial.print("\n");
+    // Serial.print(s1);Serial.print(" ");Serial.print(s2);Serial.print(" ");Serial.print(s3);Serial.print(" ");
+    // Serial.print(s4);Serial.print(" ");Serial.print(s5);Serial.print(" ");Serial.print(s6);Serial.print(" ");
+    // Serial.print(s7);Serial.print(" ");Serial.print(s8);Serial.print(" ");Serial.print(s9);Serial.print("\n");
     error();
     sprint();
-
 }
--- main/sensorReader.ino
+++ main/sensorReader.ino
@@ -1,6 +1,6 @@
 void readS()
 {
-  int f = 200;
+  int f = 120;
   s1 = analogRead(A0) < f;
   s2 = analogRead(A1) < f;
   s3 = analogRead(A2) < f;
